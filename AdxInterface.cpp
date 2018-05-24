28daaa18474039d02c6ef63ba1a97d6eeb8e2ea1
John Bromley <John.Bromley@Sage.com>
2018-05-24 13:29:09 -0400

    adxinterface.cpp
    conflict1

--- AdxInterface.cpp
+++ AdxInterface.cpp
@@ -6,7 +6,7 @@
 // V124 : MC 2000.07.07
 //--------------------------------------------------------------
 //
-#include "stdafx.h"
+#include "stdafx.h12345"
 #include <ilviews/base/message.h>
 #include "AdxInterface.h"
 #include "x3centr1.h"
